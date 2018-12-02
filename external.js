181064e78958d15a9bc3a14e2fb01ef9e2279d26
Robin Millette <robin@millette.info>
2018-12-02 07:56:57 -0500

    Comment external.js script.

--- external.js
+++ external.js
@@ -7,9 +7,20 @@ const got = require('got')
 const { prep, listen } = require('.')
 
 const fastify = prep()
-fastify.get('/thru-external', async (request, reply) => got.stream(`http://external:3000/`))
+
+// Proxy examples
+// hostnames are provided by docker
+
+// Proxy thru self, using own hostname
 fastify.get('/thru-self', async (request, reply) => got.stream(`http://${process.env.HOSTNAME}:3000/`))
-fastify.get('/thru-internal', async (request, reply) => got.stream(`http://internal:3000/`))
+
+// Proxy thru self, using "external" hostname
+fastify.get('/thru-external', async (request, reply) => got.stream(`http://external:3000/`))
+
+// Proxy thru internal server, using "internal" hostname
+fastify.get('/thru-internal', async (request, reply) => got.stream(`http://internal:3000/self2`))
+
+// A few direct routes
 fastify.get('/', async (request, reply) => ({ hello: 'world' }))
 fastify.get('/self', async (request, reply) => ({ hello: 'self-a' }))
 fastify.get('/self3', async (request, reply) => ({ hello: 'self3' }))
