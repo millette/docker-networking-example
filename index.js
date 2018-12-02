1ea15f015aec62ee474a93e10fe3218d72293da4
Robin Millette <robin@millette.info>
2018-12-02 07:56:24 -0500

    Clean up, comment lib.

--- index.js
+++ index.js
@@ -3,19 +3,15 @@
 // npm
 const fastify = require('fastify')
 
-const prep = () => {
-  const fastify666 = fastify({ logger: true })
-  const backendHeader = `x-backend-v2-${Math.floor(Math.random() * 1000)}`
-  fastify666.addHook('onSend', async (request, reply) => {
-    reply.header(backendHeader, process.env.HOSTNAME)
-  })
-  return fastify666
-}
+const backendHeader = `x-backend-${Math.floor(Math.random() * 1000)}`
 
-const listen = (fast) =>
-fast.listen(3000, process.env.HOSTNAME)
+// Add special header indicating which backend is responding
+const prep = () => fastify({ logger: true }).addHook('onSend', async (request, reply) => {
+  reply.header(backendHeader, process.env.HOSTNAME)
+})
+
+// HOSTNAME is provided by docker
+const listen = (fast) => fast.listen(3000, process.env.HOSTNAME)
   .then((address) => `server listening on ${address} / ${process.env.HOSTNAME}`)
 
-module.exports = {
-  prep, listen
-}
+module.exports = { prep, listen }
