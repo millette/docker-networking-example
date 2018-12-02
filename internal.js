2f431c5865e7b0bd2c8f34e03e2478f3173ae868
Robin Millette <robin@millette.info>
2018-12-02 07:56:47 -0500

    Comment internal.js script.

--- internal.js
+++ internal.js
@@ -5,9 +5,11 @@ const { prep, listen } = require('.')
 
 const fastify = prep()
 
+// These are only available within docker
 fastify.get('/', async (request, reply) => ({ hello: 'world' }))
 fastify.get('/self', async (request, reply) => ({ hello: 'self-b' }))
 fastify.get('/self2', async (request, reply) => ({ hello: 'self2' }))
+fastify.get('/cant', async (request, reply) => ({ hello: 'cant' }))
 
 listen(fastify)
   .then(console.log)
